0542bef94d899292913140e24be1834abadb743f
Jonathan Queiroz <dev@johnjq.com>
2020-02-15 23:48:37 -0800

    emscripten: implement workaround for CERTIFICATE_VERIFY_FAILED

--- pkgs/development/compilers/emscripten/default.nix
+++ pkgs/development/compilers/emscripten/default.nix
@@ -28,6 +28,8 @@ stdenv.mkDerivation {
     sed -i -e "s,EM_CONFIG = '~/.emscripten',EM_CONFIG = '$out/${appdir}/config'," $out/${appdir}/tools/shared.py
     sed -i -e 's,^.*did not see a source tree above the LLVM.*$,      return True,' $out/${appdir}/tools/shared.py
     sed -i -e 's,def check_sanity(force=False):,def check_sanity(force=False):\n  return,' $out/${appdir}/tools/shared.py
+    sed -i '10 i import ssl\nssl._create_default_https_context = ssl._create_unverified_context' $out/${appdir}/tools/system_libs.py
+
     # fixes cmake support
     sed -i -e "s/print \('emcc (Emscript.*\)/sys.stderr.write(\1); sys.stderr.flush()/g" $out/${appdir}/emcc.py
     mkdir $out/bin
